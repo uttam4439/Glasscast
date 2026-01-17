9db90827b89959c084cce965d3e380498f7d6738
UTTAM KUMAR <rajuttam4439@gmail.com>
2026-01-17 15:59:13 +0530

    Initial Commit

---
+++ Glasscast/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Glasscast
+//
+//  Created by UTTAM KUMAR on 17/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Glasscast/GlasscastApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlasscastApp.swift
+//  Glasscast
+//
+//  Created by UTTAM KUMAR on 17/01/26.
+//
+
+import SwiftUI
+
+@main
+struct GlasscastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
